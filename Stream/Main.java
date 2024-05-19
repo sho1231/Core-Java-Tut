fcd3bf0c1d4c25e2af1f53f45455be9a1b84f73d
Shourja Ganguly <shourja.ganguly181@gmail.com>
2024-05-19 13:16:11 +0530

    Create Main.java

---
+++ Stream/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> l = Arrays.asList(1,2,3,4,5,6,4);
+
+        // l.stream().map(n->{}).forEach(n->System.out.println(n));
+    }
+}
